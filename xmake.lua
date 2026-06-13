58ab3543b269b87a937006344f20af0ad8b7f686
schphe <git@emin.sh>
2026-06-13 05:15:30 -0500

    new libraries

--- xmake.lua
+++ xmake.lua
@@ -1,7 +1,7 @@
 add_rules("mode.debug", "mode.release")
 
 add_requires("flecs", {configs = {debug = is_mode("debug")}})
-add_requires("box2d", "clay", "enet", "glm", "libsdl3", "libsdl3_image", "libsdl3_ttf")
+add_requires("box2d", "clay", "enet", "glm", "libsdl3", "sdl3webgpu", "wgpu-native")
 add_requires("miniaudio", "stb", "glaze")
 add_requires("luau", "luabridge3")
 
@@ -9,10 +9,10 @@ target("tankpvp")
     set_kind("binary")
     set_languages("c23", "c++23")
     set_rundir("$(projectdir)")
-    add_includedirs("include")
+    add_includedirs("include", "include/library")
     add_files("src/**.cpp")
 
-    add_packages("box2d", "clay", "enet", "flecs", "glm", "libsdl3", "libsdl3_image", "libsdl3_ttf")
+    add_packages("box2d", "clay", "enet", "flecs", "glm", "libsdl3", "sdl3webgpu", "wgpu-native")
     add_packages("miniaudio", "stb", "glaze", "luau", "luabridge3")
 
     if is_plat("macosx") then
